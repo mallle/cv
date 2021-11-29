a4c4ef3b4687765b5c97a8a637526257c08a0251
Malene <maleneklit@hotmail.com>
2021-11-29 07:10:22 +0100

    Update links page

--- pages/links/index.vue
+++ pages/links/index.vue
@@ -1,9 +1,19 @@
 <template>
   <section class="cv-section">
+    <h1 class="mt-8 mb-4">Deutsch</h1>
     <Link
-      v-for="(link, i) in links"
+      v-for="(link, i) in links.de"
       :key="i"
-      :classes="link.lang === 'de' ? 'link-primary' : 'link-secondary'"
+      classes="link-primary"
+      :href="link.href"
+    >
+      {{ link.text }}</Link
+    >
+    <h1 class="mt-16 mb-4">English</h1>
+    <Link
+      v-for="(link, i) in links.en"
+      :key="i"
+      classes="link-secondary"
       :href="link.href"
     >
       {{ link.text }}</Link
@@ -16,33 +26,32 @@ export default {
   layout: 'slim',
   data() {
     return {
-      links: [
-        {
-          href: 'https://storyways.app/de',
-          text: 'Storyways - App für Interactive Geschichten (DE)',
-          lang: 'de',
-        },
-        {
-          href: 'https://storyways.app',
-          text: 'Storyways - App for interactive stories (EN)',
-          lang: 'en',
-        },
-        {
-          href: 'https://coldcasedetectives.de',
-          text: 'Cold Case Detectives - Krimispiel (DE)',
-          lang: 'de',
-        },
-        {
-          href: 'https://fictionic.com/de',
-          text: 'Fictionic (DE)',
-          lang: 'de',
-        },
-        {
-          href: 'https://fictionic.com/',
-          text: 'Fictionic (EN)',
-          lang: 'en',
-        },
-      ],
+      links: {
+        de: [
+          {
+            href: 'https://storyways.app/de',
+            text: 'Storyways - App für Interactive Geschichten',
+          },
+          {
+            href: 'https://coldcasedetectives.de',
+            text: 'Cold Case Detectives - Krimispiel',
+          },
+          {
+            href: 'https://fictionic.com/de',
+            text: 'Fictionic - Entertainment company',
+          },
+        ],
+        en: [
+          {
+            href: 'https://storyways.app',
+            text: 'Storyways - App for interactive stories',
+          },
+          {
+            href: 'https://fictionic.com/',
+            text: 'Fictionic - Entertainment company',
+          },
+        ],
+      },
     }
   },
   head() {
